adce71ec3ec84e77637710ead8a2cf95dc8495b1
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2020-07-12 00:55:47 -0300

    organização de listas de downloads

--- apps/install-apps-apt.sh
+++ apps/install-apps-apt.sh
@@ -2,74 +2,59 @@
 pass=$1
 log=$2
 
-declare -a apps=("openjdk-8-jre-headless -y"
+declare -a apps=("openjdk-8-jre-headless -y" 
                 "python3" 
-                "python3-pip"
-                "ruby-full build-essential zlib1g-dev" 
-                "synaptic" 
+                "python3-pip" 
+                "ruby-full" 
+                "build-essential" # essencial para a construção de pacotes Debian https://packages.debian.org/pt-br/sid/build-essential
+                "zlib1g-dev" # lossless data-compression library http://zlib.net/  https://packages.ubuntu.com/focal/zlib1g-dev
+                "synaptic" # gerenciamento de pacotes APT em GTK+ https://www.nongnu.org/synaptic/
                 "git" # git (https://git-scm.com/download/linux)
-                "google-chrome-stable"
-                "brave-browser" 
+                "google-chrome-stable" # Google Chrome estável
                 "mongodb-org -y" 
                 "postgresql postgresql-contrib -y" 
-                "curl"
-                "xclip"
+                "apt-transport-https" # Https download transport for APT. https://packages.debian.org/jessie/apt-transport-https
+                "curl" # transferring data with URLs https://curl.haxx.se/
+                "wget" # Baixar arquivos atraves do HTTP, HTTPS, FTP ou FTPS https://www.gnu.org/software/wget/
+                "ca-certificates" # autenticidade das conexões SSL https://packages.debian.org/pt-br/sid/ca-certificates
+                "xclip" # Copy/Paste Command's  https://github.com/astrand/xclip
                 "solaar" # https://pwr-solaar.github.io/Solaar/
                 "lsb -y" # http://download.ebz.epson.net/dsc/du/02/DriverDownloadInfo.do?LG2=EN&CN2=&DSCMI=111235&DSCCHK=fb74fba5064100c62ec5321e3b763b68223dc521
-                "virtualbox -y"
+                "virtualbox -y" # Máquina virtual 
+                "alacarte" # Main menu: https://gitlab.gnome.org/GNOME/alacarte
+                "vivaldi-stable"
                 )
 
 echo "------------------------------------------------------------" | tee -a $log
-echo "> Starting to install apps with APT-GET:"
-echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- Starting to install apps with APT-GET" >> $log
+echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- Starting to install apps with APT-GET" | tee -a $log
 echo "------------------------------------------------------------" | tee -a $log
-
 # LISTAS DE REPOSITÓRIOS
-echo "> Registrando repositórios APT:"
-echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- Registrando repositórios APT:" | tee -a $log
-
-# pacotes auxiliares
-echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- apt -- Running: sudo apt install apt-transport-https curl wget ca-certificates --fix-broken" | tee -a $log
-sudo apt install apt-transport-https curl wget ca-certificates --fix-broken
+echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- Registering APT repositories:" | tee -a $log
 
 # chrome
-#wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
-echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- apt -- repositório Chrome listado." | tee -a $log
-
-# brave-browser
-curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
-echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
-echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- apt -- repositório brave-browser listado." | tee -a $log
+echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" | sudo tee /etc/apt/sources.list.d/google.list
+wget -qO - https://dl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- APT -- repositório Chrome listado." | tee -a $log
 
 # Mongo DB
+echo "deb [arch=amd64] https://repo.mongodb.org/apt/ubuntu/dists/bionic/mongodb-org/4.2/multiverse" | sudo tee /etc/apt/sources.list.d/mongodb.list
 wget -qO - https://www.mongodb.org/static/pgp/server-4.2.asc | sudo apt-key add -
-echo "deb [ arch=amd64 ] https://repo.mongodb.org/apt/ubuntu/dists/bionic/mongodb-org/4.2/multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-4.2.list
-echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- apt -- repositório mongodb-org listado." | tee -a $log
-
-# Mongo Compass
-# wget https://downloads.mongodb.com/compass/mongodb-compass_1.15.1_amd64.deb
-# sudo dpkg -i mongodb-compass_1.15.1_amd64.deb
-# echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- apt -- repositório mongodb-compass listado."  >> $log_apt
+echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- APT -- repositório MongoDB listado." | tee -a $log
 
 # Postgres
-wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
-sudo sh -c 'echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" >> /etc/apt/sources.list.d/pgdg.list'
-echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- apt -- repositório Postgres listado." | tee -a $log
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" | sudo tee /etc/apt/sources.list.d/postgresql.list
+wget -qO - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- APT -- repositório Postgres listado." | tee -a $log
 
-# solaar (https://launchpad.net/~solaar-unifying/+archive/ubuntu/stable)
-# https://pwr-solaar.github.io/Solaar/
-sudo add-apt-repository ppa:solaar-unifying/stable
-
-# https://launchpad.net/~mc3man/+archive/ubuntu/mpv-tests
-# https://mpv.io/
-sudo add-apt-repository ppa:mc3man/mpv-tests
-
-# Vivaldi
+# Repositório Vivaldi
+echo "deb https://repo.vivaldi.com/archive/deb/ stable main" | sudo tee /etc/apt/sources.list.d/vivaldi.list
 wget -qO- https://repo.vivaldi.com/archive/linux_signing_key.pub | sudo apt-key add -
-sudo add-apt-repository 'deb https://repo.vivaldi.com/archive/deb/ stable main' 
-sudo apt update && sudo apt install vivaldi-stable
+echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- APT -- repositório Vivaldi listado." | tee -a $log
+
+
+# solaar (https://launchpad.net/~solaar-unifying/+archive/ubuntu/stable  |  https://pwr-solaar.github.io/Solaar/
+echo "deb http://ppa.launchpad.net/solaar-unifying/stable/ubuntu focal main" | sudo tee /etc/apt/sources.list.d/solaar-unifying.list
+echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- APT -- repositório Unifying listado." | tee -a $log
 
 # instalações APT
 sudo apt-get update | tee -a $log
@@ -81,5 +66,27 @@ do
     echo "> Running: sudo -S apt-get install $app -y -V"
     echo -e "$pass\n" | sudo -S apt-get install $app -y -V  | tee -a $log
     
-    echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- apt-get -- $app instalado com sucesso"  >> $log
+    echo "$(date '+%Y/%m/%d-%H:%M:%S:%N') -- INFO -- APT -- $app instalado com sucesso"  >> $log
 done
+
+# Resolver pacotes quebrados na instalação de muitos pacotes
+sudo apt-get --fix-broken install
+
+# Outra forma de corrigir a quebra de pacotes
+sudo apt-get clean
+sudo apt-get install -f
+sudo dpkg --configure -a
+sudo apt-get update
+
+# Resolver problemas com travamento do gerenciador de pacotes
+sudo rm /var/lib/apt/lists/* -vf
+sudo apt-get update
+
+# Resolver comflitos
+sudo apt-get clean
+sudo apt-get autoclean
+sudo apt-get autoremove
+
+# Resolver depêndencia quebrada, em falta ou corrompida
+sudo dpkg --configure -a
+sudo apt-get update
