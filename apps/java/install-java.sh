2ec29cfe1e5edaf43af6393f0da8a1cf946fdb33
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2021-05-18 00:52:10 -0300

    install maven

--- apps/java/install-java.sh
+++ apps/java/install-java.sh
@@ -3,5 +3,11 @@
 sudo apt-get update
 sudo apt-get dist-upgrade
 sudo apt autoremove
+
 sudo apt-get install openjdk-11-jdk-headless  -y -V
 sudo apt-get install openjdk-11-jre-headless  -y -V
+
+# maven install
+
+sudo apt-get -y install maven
+
