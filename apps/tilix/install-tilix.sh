5631025d2d4b1e7393fcf1984c5764c0f08faddb
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2021-06-03 01:37:59 -0300

    tilix

---
+++ apps/tilix/install-tilix.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:webupd8team/terminix
+sudo apt-get update
+udo apt install tilix
---
+++ apps/tilix/remove-tilix.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+sudo add-apt-repository ppa:webupd8team/terminix -r -y
+sudo apt-get remove tilix --auto-remove
