ae23161a93d9cb8a464b1328086de1b677e5c110
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2021-12-19 15:51:04 -0300

    box

---
+++ apps/box/install-box.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get install davfs2
+
+wget https://github.com/alfredobonino/Box2Tux/archive/master.zip
+
+unzip master.zip
+
+cd Box2Tux-master/
+
+chmod +x box2tux
+
+./box2tux
+
+
+# https://www.edivaldobrito.com.br/como-montar-seu-armazenamento-box-com-em-um-desktop-ubuntu/
