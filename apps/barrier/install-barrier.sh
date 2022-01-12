1084261a231ac1492993a410c0f96749840a96db
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-01-12 10:31:09 -0300

    barrier e scrcpy

---
+++ apps/barrier/install-barrier.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+sudo snap install barrier
+
+# https://github.com/debauchee/barrier
+# https://snapcraft.io/barrier
---
+++ apps/scrcpy/install-scrcpy.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# Requisito do SDK Android
+
+sudo snap install scrcpy
+
+# https://snapcraft.io/scrcpy
+# https://github.com/Genymobile/scrcpy
