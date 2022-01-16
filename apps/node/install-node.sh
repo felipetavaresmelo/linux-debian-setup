2f47d0797ad8198e0a8f99da494253fb7672110c
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-01-16 09:20:57 -0300

    Node

--- apps/node/install-node.sh
+++ apps/node/install-node.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash -
 sudo apt-get install -y nodejs
 
 #https://github.com/nodesource/distributions/blob/master/README.md
