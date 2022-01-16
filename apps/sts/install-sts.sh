8c9948dc5ce563326c3324fa7112e56020640f19
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-01-16 09:00:27 -0300

    Spring Tool Suite

--- apps/sts/install-sts.sh
+++ apps/sts/install-sts.sh
@@ -1,22 +1,24 @@
-mkdir $HOME/applications/sts
+mkdir $HOME/apps/sts
 
-cd $HOME/applications/sts
+cd $HOME/apps/sts
 
-wget https://download.springsource.com/release/STS4/4.10.0.RELEASE/dist/e4.19/spring-tool-suite-4-4.10.0.RELEASE-e4.19.0-linux.gtk.x86_64.tar.gz
+# wget https://download.springsource.com/release/STS4/4.10.0.RELEASE/dist/e4.19/spring-tool-suite-4-4.10.0.RELEASE-e4.19.0-linux.gtk.x86_64.tar.gz
 
-sudo tar -xvf spring-tool-suite-4-4.10.0.RELEASE-e4.19.0-linux.gtk.x86_64.tar.gz
+wget https://download.springsource.com/release/STS4/4.13.0.RELEASE/dist/e4.22/spring-tool-suite-4-4.13.0.RELEASE-e4.22.0-linux.gtk.x86_64.tar.gz
 
-rm -f spring-tool-suite-4-4.10.0.RELEASE-e4.19.0-linux.gtk.x86_64.tar.gz
+sudo tar -xvf spring-tool-suite-4-4.13.0.RELEASE-e4.22.0-linux.gtk.x86_64.tar.gz
 
-sudo chmod -R 777 sts-4.10.0.RELEASE
+rm -f spring-tool-suite-4-4.13.0.RELEASE-e4.22.0-linux.gtk.x86_64.tar.gz
+
+sudo chmod -R 777 sts-4.13.0.RELEASE
 
 cd $HOME/.local/share/applications/
 
 sudo echo "[Desktop Entry]
 Name=Spring Tool Suite
 Comment=Spring Tool Suite
-Exec=~/sts/sts-4.10.0.RELEASE/SpringToolSuite4
-Icon=~/sts/sts-4.10.0.RELEASE/icon.xpm
+Exec=/home/felipe/apps/sts/sts-4.13.0.RELEASE/SpringToolSuite4
+Icon=/home/felipe/apps/sts/sts-4.13.0.RELEASE/icon.xpm
 StartupNotify=true
 Terminal=false
 Type=Application
