f12a73a029099fb932d6d10cc8383856e78ffd30
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-01-30 00:12:04 -0300

    yarn

--- apps/yarn/install-yarn.sh
+++ apps/yarn/install-yarn.sh
@@ -1,3 +1,11 @@
 #!/bin/sh
 
-sudo npm install --global yarn
+
+ curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+
+ echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+
+ sudo apt-get update && sudo apt-get install yarn
+
+
+# sudo npm install --global yarn
