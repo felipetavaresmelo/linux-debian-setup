fb2cb91fca777022db14c7569980ee8c4de60687
Felipe Tavares de Melo <felipetavaresmelo@gmail.com>
2022-11-06 17:45:51 -0300

    Update install-yarn.sh

--- apps/development/yarn/install-yarn.sh
+++ apps/development/yarn/install-yarn.sh
@@ -2,7 +2,6 @@
 
 sudo apt-get update
 sudo apt-get dist-upgrade -y
-sudo apt autoremove
 
 curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | gpg --dearmor | sudo tee /usr/share/keyrings/yarnkey.gpg >/dev/null
 echo "deb [signed-by=/usr/share/keyrings/yarnkey.gpg] https://dl.yarnpkg.com/debian stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
